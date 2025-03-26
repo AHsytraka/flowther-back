c461e2f617ef224b4a35af82e834b2209daa6753
AHsytraka <ahsytraka@gmail.com>
2025-03-26 10:42:33 +0300

    feat(socket): socket setup

---
+++ src/modules/socket/socket.gateway.ts
@@ -0,0 +1,20 @@
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Socket;
+
+  handleConnection(client: any, ...args: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  handleDisconnect(client: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): string {
+    return 'Hello world!';
+  }
+}
